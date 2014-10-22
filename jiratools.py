7951baf02867e833197ced9d9052474bbc78a4dd
fezick <jason@directemployersfoundation.org>
2014-10-22 11:53:56 -0400

    Automatically assign unassigned INDEXREP tickets to a dev in the content-acquisition group. ST-582

--- jiratools.py
+++ jiratools.py
@@ -1,9 +1,10 @@
 # This script shows how to connect to a JIRA instance with a
 # username and password over HTTP BASIC authentication.
 
-import logging
-from jira.client import JIRA
 from datetime import datetime
+from jira.client import JIRA
+import logging
+import random
 import secrets
 
 
@@ -19,11 +20,12 @@ class Housekeeping():
                             basic_auth=secrets.housekeeping_auth) 
     
         # commands to run
-        self.content_acquisition_auto_qc()
+        #self.content_acquisition_auto_qc()
 
     def content_acquisition_auto_qc(self):
         # Get some issues
-        issues = self.jira.search_issues('project=INDEXREP and status=Merged and updated<="-30m"')
+        issues = self.jira.search_issues(
+            'project=INDEXREP and status=Merged and updated<="-30m"')
 
         # Iterate on the issues
         for issue in issues:
@@ -31,5 +33,24 @@ class Housekeeping():
             message = '[~%s], this issue is ready for QC.' % reporter
             self.jira.transition_issue(issue.key,'771')
             self.jira.add_comment(issue.key, message)
+
+    def random_auto_assign(self):
+        ca_group = self.jira.groups(
+            query='content-acquisition'
+            )['groups'][0]['name']
+        members = self.jira.group_members(ca_group)
+        issues = self.jira.search_issues(
+            'project=INDEXREP and (assignee=EMPTY OR assignee=housekeeping) and \
+             status in (open,reopened)')
+        
+        # iterate over the issues and randomly assign them to a user 
+        for issue in issues:
+            ran_dev = random.choice(members.keys())
+            reporter = issue.fields.reporter.key
+            self.jira.assign_issue(issue=issue,assignee=ran_dev)
+            message = ("[~%s], this issue has been automically assigned "
+                "to [~%s].") % (reporter,ran_dev)
+            self.jira.add_comment(issue.key, message)
+
 
 Housekeeping()
