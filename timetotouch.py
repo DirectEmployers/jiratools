bd584511cb4434bfe780469d419ec9236f6a64b9
Jason Sole <jason@directemployers.org>
2020-02-05 10:10:16 -0500

    Add time to touch calculator

---
+++ timetotouch.py
@@ -0,0 +1,141 @@
+from datetime import datetime
+from jira.client import JIRA
+import logging
+import operator
+import secrets
+import settings
+import statistics
+
+class TimeToTouch:
+    def __init__(self):
+        # open JIRA API Connection
+        options=secrets.options
+        housekeeping_auth=secrets.housekeeping_auth
+        self.jira = JIRA(options=options, basic_auth=housekeeping_auth)
+        self.calculate_touch_time("Sales-Engineering")
+
+    def calculate_touch_time(self,team):
+        """
+        Calculates Mean and Median time to touch for tasks.
+
+        Inputs:
+            :team:  Jira Group Name for the team
+
+        Returns: Console Print
+
+        """
+        # init the issue container
+        issues = []
+
+        # get the issues. Can only do it 100 at a time, so loop-the-loop
+        jql = self.jira.filter(secrets.time_to_touch_filters[team]).jql
+        start=1
+        max=100
+        tempIssues = self.get_issues(jql,start,max)
+        issues = tempIssues
+        start=start+max
+        while len(tempIssues)==max:
+            tempIssues = self.get_issues(jql,start,max)
+            start=start+max
+            issues = issues+tempIssues
+
+        # init the list to contain the touch time
+        touchList = []
+        # get SE user list
+        userList = self.get_group_members(team)
+        for issue in issues:
+            createdDate=datetime.strptime(
+                issue.fields.created.split(".")[0],
+                "%Y-%m-%dT%H:%M:%S"
+                )
+            ticketLog = issue.changelog.histories # get the ticket changelog
+
+            # init the dict that will contain the ticket key, author, and time to touch
+            touchDict = {}
+            touchDict["issue"]=issue.key
+
+            logCount = ticketLog.__len__()
+            i = logCount-1
+            countItem=False
+            while i >= 0:
+                author = ticketLog.__getitem__(i).author
+                if author.key in userList:
+                    seTouchDate=datetime.strptime(
+                        ticketLog.__getitem__(i).created.split(".")[0],
+                        "%Y-%m-%dT%H:%M:%S"
+                        )
+                    seTouch = seTouchDate-createdDate
+                    seTouchHours = round(seTouch.total_seconds()/60/60,2)
+                    touchDict["User"]=author.key
+                    touchDict["touchTime"]=seTouchHours
+                    countItem=True
+                    i=-1
+                else:
+                    i=i-1
+            if(countItem):
+                touchList.append(touchDict)
+
+        total=0
+        touchTimeData = []
+        for touch in touchList:
+            touchTimeData.append(touch["touchTime"])
+
+        # buils the output strings and print to console.
+        touchAverage = round(statistics.mean(touchTimeData),2)
+        touchAverage = self.min_hr_switch(touchAverage)
+        touchMedian = round(statistics.median(touchTimeData),2)
+        touchMedian = self.min_hr_switch(touchMedian)
+        print("{}: ".format(team))
+        print("    Average: {}".format(touchAverage))
+        print("    Median: {}".format(touchMedian))
+        print()
+
+
+    def get_issues(self,jql,start,max):
+        """
+        Returns issues found using a jira filter.  Supports max and start values as the
+        Jira API has a hard limit on results and requires looped queries to get all issues
+        when the count is higher than that.
+
+        Inputs:
+            :jql:   the dict key for the filter in settings
+            :start: Start point for the query (to support followup requests)
+            :max:   Max results. Usually 100, based on API limits
+
+        Returns:
+            :issues:    Jira Issues object
+
+        """
+        result = self.jira.search_issues(
+            jql,
+            startAt=start,
+            expand="changelog",
+            maxResults=max
+            )
+        return result
+
+    def min_hr_switch(self,touchTime):
+        """
+        Switches between hours and minutes for readability
+        Inputs:
+            :touchTime: Hour measurement. Float
+
+        Returns:
+            :touchTime: String statement of time interval
+
+        """
+        if touchTime < 1:
+            touchTime = str(touchTime*60) + " Minutes"
+        else:
+            touchTime = str(touchTime) + " Hours"
+        return touchTime
+
+    def get_group_members(self, group_name):
+        """
+        Returns the members of a group as a list
+        """
+        group = self.jira.groups(query=group_name)[0]
+        members = self.jira.group_members(group)
+        return members
+
+TimeToTouch()
